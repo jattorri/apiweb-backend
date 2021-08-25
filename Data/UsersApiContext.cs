5d187d072d86413e070cc068a16eb2904146c0dc
Juan Attorri <jattorri@gmail.com>
2021-08-25 09:49:39 -0300

    init

---
+++ Data/UsersApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UsersApi.Models;
+
+namespace UsersApi.Data
+{
+    public class UsersApiContext : DbContext
+    {
+        public UsersApiContext (DbContextOptions<UsersApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsersApi.Models.User> User { get; set; }
+    }
+}
